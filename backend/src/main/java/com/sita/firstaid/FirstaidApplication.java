95cf6a0efa3dcbcc142966e1c0fbbdf8a0d4cf59
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2025-04-06 17:19:53 -0400

    init commit

---
+++ backend/src/main/java/com/sita/firstaid/FirstaidApplication.java
@@ -0,0 +1,13 @@
+package com.sita.firstaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstaidApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sita/firstaid/FirstaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sita.firstaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
